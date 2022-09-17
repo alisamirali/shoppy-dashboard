9334db8fd31a8ea3ac9625e2fce54afdea62d7b7
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-09-17 10:03:19 +0200

    Update ThemeSettings.jsx

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -12,7 +12,7 @@ const ThemeSettings = () => {
 
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-[85%] md:w-400 p-2">
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-[100%] md:w-400 p-2">
         <div className="flex justify-between items-center p-4 ml-4">
           <p className="font-semibold text-lg">Settings</p>
           <button
